bb7e45fb744f0e77201a8748e755ba6f2f8e51f3
Samee ul haq <samisamee110@gmail.com>
2025-12-24 11:21:28 +0530

    CRUD with authentication completed

--- controllers/postController.js
+++ controllers/postController.js
@@ -15,3 +15,35 @@ try {
     return res.status(500).json({message:"Error creating post",error})
 }
 }
+
+export const getUserPosts =async (req,res)=>{
+try {
+    const userId = req.user.id
+    
+    const personalPosts=await Post.find({
+        user:userId
+    })
+    res.status(200).json(personalPosts)
+} catch (error) {
+    res.status(500).json({message:"Error creating post",error})
+}
+}
+
+export const deletePost =async (req,res)=>{
+    try {
+        const id=req.params.id
+        const postToDelete= await Post.findById(id)
+    
+        if(!postToDelete)
+            return res.status(404).json({message:"Post not found"})
+    
+        if(postToDelete.user.toString() !== req.user.id)
+            return res.status(403).json({message:"Forbidden"})
+    
+        await postToDelete.deleteOne()
+        res.status(200).json({message:"Deleted successfully"})
+        
+    } catch (error) {
+        res.status(500).json({ message: "Error deleting post", error });
+    }
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,14 @@
 import express from 'express'
-import { createPost } from '../controllers/postController.js'
+import { createPost,
+         getUserPosts,
+         deletePost
+ } from '../controllers/postController.js'
 import { verifyToken } from '../middleware/authMiddleware.js'
 
 const postRouter=express.Router()
 
 postRouter.post('/',verifyToken,createPost)
+postRouter.post('/',verifyToken,getUserPosts)
+postRouter.post('/:id',verifyToken,deletePost)
 
 export default postRouter
